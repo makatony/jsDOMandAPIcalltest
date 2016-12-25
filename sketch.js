ad645597756275e5939136f6ee37efb142d421bc
Tony Kumin <makatony@gmail.com>
2016-12-25 18:48:36 +0100

    refactor

--- sketch.js
+++ sketch.js
@@ -1,17 +1,56 @@
 var randomText;
+var spacepeople;
+loadCallback = function(){ };
+loadErrorCallback = function(e){ alert("a"); console.log("error"); };
+
 
 function preload() {
-  var url = 'http://www.randomtext.me/api/';
-  randomText = loadJSON(url);
+	var url = 'http://www.randomtext.me/api/'; //this API doesnt support JSONP
+	randomText = loadJSON(url,loadCallback,loadErrorCallback,"json");
+	var url = 'http://api.open-notify.org/astros.json';
+	spacepeople = loadJSON(url,loadCallback,loadErrorCallback,"jsonp");
 }
 
 
 function setup() {
 	noCanvas();
-	randomTextP = createP("randomText");
-	randomTextP.html(randomText.text_out);
-	Object.assign(randomText, { a: { b: "text", c: "some text" } })
-	console.log(JSON.stringify(randomText, null, 4));
+	if (randomText) {
+		randomTextP = createP("randomText");
+		randomTextP.html(randomText.text_out);
+		Object.assign(randomText, { a: { b: "text", c: "some text" } })
+		console.log(printR(randomText));
+	}
 	
+	if (spacepeople) {
+		spacePeopleP = createP("spacepeople");
+		spacePeopleP.html(printR(spacepeople));
+		print(printR(spacepeople));
+	}
+	
+	var someIDP = select("#someID");
+	someIDP.html("aaa");
+	someIDP.style("color: red");
+	
+	var someClassP = selectAll(".someClass");
+	someClassP.forEach(function (elt) {
+		elt.style("color: red");
+		elt.mouseOver(mouseOverEvent);
+		console.log(printR(elt));
+	});
+	
+	console.log(printR(someClassP));
+	console.log(someClassP);
+	
+	createA("https://www.quora.com/What-is-the-difference-between-JSON-and-JSONP","JSON vs JSONP");
+	
+}
+
+function mouseOverEvent() {
+	this.style("color: pink");
+}
+
+
+function printR(obj) {
+	return JSON.stringify(obj, null, 4);
 }
 
